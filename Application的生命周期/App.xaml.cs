51f328f959dc13af259119819ed0372c09af9277
TAN-HOME-PC\Feet <59450804@qq.com>
2017-04-14 13:21:59 +0800

    新建仓库

---
+++ Application的生命周期/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Application的生命周期
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// 程序运行时
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            System.Windows.Forms.MessageBox.Show("我打开了");
+        }
+
+        /// <summary>
+        /// 程序关机前调用
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            base.OnSessionEnding(e);
+            System.Windows.Forms.MessageBox.Show("系统关机时调用");
+        }
+
+        /// <summary>
+        /// 程序关闭前调用
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            System.Windows.Forms.MessageBox.Show("我离开了");
+        }
+
+        /// <summary>
+        /// 程序获得焦点
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            System.Windows.Forms.MessageBox.Show("我在焦点");
+        }
+
+        /// <summary>
+        /// 程序失去焦点
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            System.Windows.Forms.MessageBox.Show("我失去了焦点");
+
+        }
+    }
+}
---
+++ 全局异常获取/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace 全局异常获取
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //注册Application_Error
+                         this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+        }
+
+        //异常处理逻辑
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("异常信息" + e.Exception.Message);
+            //处理完后，我们需要将Handler=true表示已此异常已处理过
+            e.Handled = true;
+        }
+    }
+}
